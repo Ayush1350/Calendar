b072b5d9b830b4088ee5d4911f28d9200ff4f457
Ayush Patel <ayushpatel2492002@gmail.com>
2024-09-02 16:07:00 +0530

    Added Date Picker

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { IoIosArrowBack } from "react-icons/io";
 import { IoIosArrowForward } from "react-icons/io";
+import { CiCalendar } from "react-icons/ci";
 
-import "./App.css";
 import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
   const [dates, setDates] = useState([]);
@@ -10,6 +11,7 @@ function App() {
   const [todayDate, setToDayDate] = useState(d.getDate());
   const [todayMonth, setTodayMonth] = useState(d.getMonth());
   const [todayYear, setTodayYear] = useState(d.getFullYear());
+  const [selectedDate, setSelectedDate] = useState(null);
 
   const monthName = new Date(todayYear, todayMonth).toLocaleString("en-US", {
     month: "short",
@@ -27,11 +29,11 @@ function App() {
       const prevYear = month === 0 ? year - 1 : year;
 
       const daysInPrevMonth = getDaysInMonth(prevYear, prevMonth);
-      console.log(daysInPrevMonth);
       const daysInCurrentMonth = getDaysInMonth(year, month);
+      console.log(daysInCurrentMonth);
 
       const firstDayOfMonth = new Date(year, month, 1).getDay();
-      const totalCells = 35;
+      const totalCells = 42;
 
       for (let i = firstDayOfMonth - 1; i >= 0; i--) {
         dates.push({
@@ -61,6 +63,10 @@ function App() {
     setDates(generateCalendarDates(todayYear, todayMonth));
   }, [todayMonth, todayYear]);
 
+  useEffect(() => {
+    setSelectedDate(todayDate);
+  }, [setSelectedDate]);
+
   const handleLeftClick = () => {
     if (todayMonth === 0) {
       setTodayMonth(11);
@@ -79,53 +85,77 @@ function App() {
     }
   };
 
+  const handleDateClick = (date) => {
+    setSelectedDate(date);
+  };
+
+  const calenderIconClick = () => {
+    alert(
+      `Selected Date Is ${selectedDate} / ${todayMonth + 1} / ${todayYear}`
+    );
+  };
+
   return (
-    <>
+    <div className="flex items-center gap-[3rem]">
       <div className="bg-[#FFFFFF] w-[20rem] h-[20rem] mt-[6rem] rounded-[1rem] p-[1rem]">
         <div className="flex items-center justify-between">
-          <span className="font-black text-[1.2rem]">
+          <span className="font-bold text-[1.2rem]">
             {monthName} {todayYear}
           </span>
           <div className="flex gap-[1rem]">
             <IoIosArrowBack
               onClick={handleLeftClick}
-              className={`hover:cursor-pointer ${
+              className={`hover:cursor-pointer text-[1.2rem] ${
                 todayMonth >= d.getMonth() ? "text-gray-950" : "text-gray-400"
               }`}
             />
             <IoIosArrowForward
               onClick={handleRightClick}
-              className={`hover:cursor-pointer ${
+              className={`hover:cursor-pointer text-[1.2rem] ${
                 todayMonth <= d.getMonth() ? "text-gray-950" : "text-gray-400"
               }`}
             />
           </div>
         </div>
         <div className="flex justify-around mt-[1rem] font-semibold">
+          <span>Su</span>
           <span>Mo</span>
           <span>Tu</span>
           <span>We</span>
           <span>Th</span>
           <span>Fr</span>
           <span>Sa</span>
-          <span>Su</span>
         </div>
-        <div className="grid grid-rows-5 grid-cols-7 w-[18rem] h-[75%] m-[0.2rem] border-solid border-[1px] border-[#D5D4DF]">
+        <div className="grid grid-rows-6 grid-cols-7 w-[18rem] h-[75%] m-[0.2rem] border-solid border-[1px] border-[#D5D4DF]">
           {dates?.map((item, index) => (
             <p
               key={index}
               className={`flex justify-center items-center border-solid border-[1px] border-[#D5D4DF] hover:cursor-pointer ${
-                item.date === todayDate && item.currentMonth
+                item.date === selectedDate && item.currentMonth
                   ? "bg-[#45539D] text-white"
                   : "hover:bg-[#E9F0F5]"
               } ${item.currentMonth ? "" : "text-gray-400"}`}
+              onClick={() => handleDateClick(item.date)}
             >
               {item.date}
             </p>
           ))}
         </div>
       </div>
-    </>
+      <div className="relative">
+        <span className="flex items-center justify-between w-[12rem] h-[3rem] bg-[#414E55] text-[#cbcbcb] mt-[3rem] p-[0.8rem] border-[1px] border-black rounded-[0.3rem]">
+          <p>{`${selectedDate} / ${todayMonth + 1} / ${todayYear}`}</p>
+
+          <CiCalendar
+            className="text-[1.3rem] hover:cursor-pointer"
+            onClick={calenderIconClick}
+          />
+        </span>
+        <p className="absolute top-[35%] left-[5%] bg-[#414E55] text-[#cbcbcb] ">
+          Date Picker
+        </p>
+      </div>
+    </div>
   );
 }
 
